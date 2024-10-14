14fd99baf6439678b154b0db6bf6868baf368683
FMisi <felegyimisi@gmail.com>
2024-10-14 17:04:25 +0200

    Update

---
+++ zh/zh/Players.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zh
+{
+    internal class Players
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Nationality { get; set; }
+        public int Overall { get; set; }
+        public string BestPosition { get; set; }
+        public string Club { get; set; }
+        public int ValueEUR { get; set; }
+    }
+}
